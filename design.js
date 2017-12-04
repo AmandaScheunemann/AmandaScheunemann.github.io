d1cdea381afe6584c65e98c17a0c616ee63fc1aa
Realiza <realiza@Realiza-4.local>
2017-12-04 15:45:19 -0200

    Pagina Inicial semipronta

--- design.js
+++ design.js
@@ -1,1 +1,15 @@
 //https://github.com/AmandaScheunemann/design_de_interiores.git
+$(document).ready(function () {
+
+    $('#wpp').hover(function () {
+        $('.contatowpp').show();
+    });
+
+
+
+
+    $(".servicos").hover(function(){
+        $("#principal").hide();
+    });
+
+});
