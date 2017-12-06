b23a656af32055c2e649dfe69e051647438df368
Realiza <realiza@Realiza-4.local>
2017-12-06 14:23:22 -0200

    jhskj

--- design.js
+++ design.js
@@ -6,10 +6,22 @@ $(document).ready(function () {
     });
 
 
-
-
     $(".servicos").hover(function(){
         $("#principal").hide();
+        $(".divServicos").show();
+        $("divProdutos").hide();
+        $("divAmbientes").hide();
     });
-
+    $(".produtos").hover(function(){
+        $("#principal").hide();
+        $(".divServicos").hide();
+        $(".divProdutos").show();
+        $("divAmbientes").hide();
+    });
+    $(".ambientes").hover(function(){
+        $("#principal").hide();
+        $(".divServicos").hide();
+        $(".divProdutos").hide();
+        $("divAmbientes").show();
+    });
 });
